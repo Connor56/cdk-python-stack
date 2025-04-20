52b4277e197f830f09b63961a24ee195f2efcf38
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2025-04-20 09:57:40 +0100

    refactor: add more comments to explain the different parts of the stack

--- my_first_python_stack/my_first_python_stack_stack.py
+++ my_first_python_stack/my_first_python_stack_stack.py
@@ -1,5 +1,4 @@
 from aws_cdk import (
-    # Duration,
     Stack,
     aws_lambda as _lambda,
 )
@@ -11,27 +10,33 @@ class MyFirstPythonStackStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Base lambda function set up
         fn = _lambda.Function(
             self,
-            "MyFunction",
-            runtime=_lambda.Runtime.PYTHON_3_11,
-            code=_lambda.Code.from_asset("lambda"),
-            handler="lambda_function.lambda_handler",
+            "MyFunction",  # The name of the function in Cloud Formation
+            runtime=_lambda.Runtime.PYTHON_3_11,  # You runtime environment
+            code=_lambda.Code.from_asset("lambda"),  # Your code directory
+            handler="lambda_function.lambda_handler",  # Your <module>.<handler-function>
         )
 
         # Create a function URL for the Lambda function
         fn_url = fn.add_function_url(
-            auth_type=_lambda.FunctionUrlAuthType.NONE,  # Public access
+            auth_type=_lambda.FunctionUrlAuthType.NONE,  # Public access, no Auth required
             cors=_lambda.FunctionUrlCorsOptions(
-                allowed_origins=["*"],
-                allowed_methods=[_lambda.HttpMethod.ALL],
-                allowed_headers=["*"],
+                allowed_origins=[
+                    "*"
+                ],  # Allow the request to come from any origin
+                allowed_methods=[
+                    _lambda.HttpMethod.ALL
+                ],  # Allow the request to have any http method
+                allowed_headers=["*"],  # Allow any headers in the request
             ),
         )
 
         # Output the function URL
         from aws_cdk import CfnOutput
 
+        # Print the URL address to the terminal when you deploy
         CfnOutput(
             self,
             "FunctionUrl",
