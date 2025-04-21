aeeef9a04eb4cc4433a071dbd675762e14c83937
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2025-04-21 21:18:40 +0100

    feat: split functions again and allow one lambda to invoke the other

--- my_first_python_stack/my_first_python_stack_stack.py
+++ my_first_python_stack/my_first_python_stack_stack.py
@@ -1,48 +1,80 @@
 from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
+    aws_iam as iam,
     CfnOutput,
 )
 from constructs import Construct
 
 
 class MyFirstPythonStackStack(Stack):
+    """
+    Two Lambda functions and dedicated URLS. One function with
+    permission to invoke the other.
+    """
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        functions = [
-            {
-                "id": "MyFunction",
-                "handler": "lambda_function.lambda_handler",
-            },
-            {
-                "id": "MyFunction2",
-                "handler": "lambda_function_2.lambda_handler",
-            },
-        ]
+        # Create a function
+        fn = _lambda.Function(
+            self,
+            "MyFunction",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            code=_lambda.Code.from_asset("lambda"),
+            handler="lambda_function.lambda_handler",
+        )
 
-        for cfg in functions:
-            fn = _lambda.Function(
-                self,
-                cfg["id"],
-                runtime=_lambda.Runtime.PYTHON_3_11,
-                code=_lambda.Code.from_asset("lambda"),
-                handler=cfg["handler"],
-            )
+        fn_url = fn.add_function_url(
+            auth_type=_lambda.FunctionUrlAuthType.NONE,
+            cors=_lambda.FunctionUrlCorsOptions(
+                allowed_origins=["*"],
+                allowed_methods=[_lambda.HttpMethod.ALL],
+                allowed_headers=["*"],
+            ),
+        )
 
-            fn_url = fn.add_function_url(
-                auth_type=_lambda.FunctionUrlAuthType.NONE,
-                cors=_lambda.FunctionUrlCorsOptions(
-                    allowed_origins=["*"],
-                    allowed_methods=[_lambda.HttpMethod.ALL],
-                    allowed_headers=["*"],
-                ),
-            )
+        # Create a function
+        fn2 = _lambda.Function(
+            self,
+            "MyFunction2",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            code=_lambda.Code.from_asset("lambda"),
+            handler="lambda_function_2.lambda_handler",
+        )
 
-            CfnOutput(
-                self,
-                f"{cfg['id']}Url",
-                value=fn_url.url,
-                description=f"URL for the {cfg['id']} Lambda function",
+        fn_url2 = fn2.add_function_url(
+            auth_type=_lambda.FunctionUrlAuthType.NONE,
+            cors=_lambda.FunctionUrlCorsOptions(
+                allowed_origins=["*"],
+                allowed_methods=[_lambda.HttpMethod.ALL],
+                allowed_headers=["*"],
+            ),
+        )
+
+        # Grant fn permission to invoke fn2
+        fn.add_to_role_policy(
+            iam.PolicyStatement(
+                actions=["lambda:InvokeFunction"],
+                resources=[fn2.function_arn],
             )
+        )
+        # This can also be done with
+        # fn.grant_invoke(fn2)
+
+        # Expose the second function's ARN to the first function
+        fn.add_environment("PEER_FN_ARN", fn2.function_arn)
+
+        CfnOutput(
+            self,
+            "MyFunctionUrl",
+            value=fn_url.url,
+            description=f"URL for the MyFunction Lambda function",
+        )
+
+        CfnOutput(
+            self,
+            "MyFunction2Url",
+            value=fn_url2.url,
+            description=f"URL for the MyFunction2 Lambda function",
+        )
