2303c75450c09464ad825bac918d24a9001c3d14
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2025-04-22 22:04:45 +0100

    feat: initialise two lambda stacks in the app.py

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
-#!/usr/bin/env python3
-import os
-
 import aws_cdk as cdk
 
-from my_first_python_stack.my_first_python_stack_stack import MyFirstPythonStackStack
-
+from my_first_python_stack.my_first_python_stack_stack import FunctionStack
 
 app = cdk.App()
-MyFirstPythonStackStack(app, "MyFirstPythonStackStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+function_2 = FunctionStack(
+    app,
+    "MyFunction2Stack",
+    handler="lambda_function_2.lambda_handler",
+)
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+FunctionStack(
+    app,
+    "MyFunctionStack",
+    handler="lambda_function.lambda_handler",
+    target_function_arn=function_2.fn.function_arn,
+)
 
 app.synth()
